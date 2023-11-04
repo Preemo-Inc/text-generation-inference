update causal batch for ct2 and fix nf4

server/text_generation_server/models/custom_modeling/flash_llama_modeling.py
@@ -185,8 +185,8 @@ class FlashLlamaAttention(torch.nn.Module):
         self.hidden_size = config.hidden_size
         self.head_size = self.hidden_size // self.num_heads
 
-        self.rotary_emb = PositionRotaryEmbedding.load(
-            prefix=f"{prefix}.rotary_emb", weights=weights
+        self.rotary_emb = PositionRotaryEmbedding.static(
+            dim=self.head_size, device=weights.device, base=10000.0, 
         )
 
         self.softmax_scale = self.head_size**-0.5